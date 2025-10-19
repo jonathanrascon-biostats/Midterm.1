23555e219ab08f8c78204b1063b22467b00da1ff
Jonathan Rascon <jonathan_rascon@brown.edu>
2025-10-19 15:44:20 -0400

    1st commit. I uploaded the data into R

---
+++ Midterm.1.R
@@ -0,0 +1,13 @@
+#MIDTERM 1
+
+#The given data set is psychiatric symptoms, secondary to an acute spinal injury
+#and associated to age. The columns are: Record ID- the unique identifier, Age 
+#Group (two- 18-35, 65-80), BSI total (brief symptom indicator), and Sig.Scale
+#which is a score associated with clinical significannce. Record ID is nominal,
+#Age Group is categorical, and BSI and Sig.Scale are ordinal--for our purposes,
+#these two should be stored as numeric.
+
+#set working directory and create Git repository------
+  #I created a repo on the github as "Midterm.1" and created a git project in R
+  #ands connected it to my online repo. Just to check the directory, I ran:
+getwd() #which returns my repo directory
