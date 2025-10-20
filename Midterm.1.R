51847fcdd9920a59fc04f324f3e43cef02740221
Jonathan Rascon <jonathan_rascon@brown.edu>
2025-10-20 19:53:00 -0400

    Both Bar plots done

--- Midterm.1.R
+++ Midterm.1.R
@@ -60,13 +60,26 @@ by(data = BSI.sig.data$Sig.Scale, BSI.sig.data$Age.Group,
 BSI.plot <- BSI.sig.data %>% 
   ggplot(aes(x = Age.Group, y = BSI.Total, fill = Age.Group)) + 
   stat_summary(fun = mean, geom = "bar", width = .7) +
-  geom_errorbar(stat = "summary", fun.data = "mean_se", width = 0.2, color = "red")+
+  geom_errorbar(stat = "summary", fun.data = "mean_se", width = 0.2, color = "black")+
   scale_y_continuous(limits = c(0, 140), breaks = seq(from = 0, to = 140, by = 5))+
   scale_fill_manual(values = c("forestgreen", "blue")) +
   labs(title = "BSI Scores by Age Group", x = "Age Group" , y = "Mean Score by Age") + 
   theme(legend.position = "none")
 
 BSI.plot  
+
+#Plot Sig.Scale mean data-------
+
+Sig.Scale.plot <- BSI.sig.data %>% 
+  ggplot(aes(x = Age.Group, y = Sig.Scale, fill = Age.Group)) +
+  stat_summary(fun = mean, geom = "bar", width = .7) +
+  geom_errorbar(stat = "summary", fun.data = "mean_se", width = 0.2, color = "tomato")+
+  scale_y_continuous(limits = c(-1, 10), breaks = seq(from = 0, to = 10, by = 1))+
+  scale_fill_manual(values = c("skyblue", "green")) +
+  labs(title = "SIG Scores by Age Group", x = "Age Group" , y = "Mean Score by Age") + 
+  theme(legend.position = "none")
+
+Sig.Scale.plot
 
 #Melt data into long(tidy) format
 BSI.sig.data.long <- melt(BSI.sig.data, id.vars = c("Record", "Age.Group"), 
--- Midtermscrap.R
+++ Midtermscrap.R
@@ -12,7 +12,8 @@ Sig.stats <- data.frame(do.call(rbind, Sig.stats))
 BSI.plot <- BSI.sig.data %>% 
     ggplot(aes(x = Age.Group, y = BSI.Total, fill = Age.Group)) + 
     stat_summary(fun = mean, geom = "bar", width = .7) +
-    stat_summary(fun = mean_se, geom = "errorbar", width = .4) +
+  #this next line is the problem
+    stat_summary(fun = mean_se, geom = "errorbar", width = .4, color = "red") +
     scale_y_continuous(limits = c(0, 140), breaks = seq(from = 0, to = 140, by = 5))+
       scale_fill_manual(values = c("forestgreen", "blue")) +
       labs(main = "BSI Scores by Age Group", x = "Age Group" , y = "Mean Score by Age") + 
